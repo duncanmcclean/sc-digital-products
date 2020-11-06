180ad83a24565b9c9f6b06c120ca5839fc8b2c47
Duncan McClean <duncan@mcclean.co.uk>
2020-11-06 18:37:42 +0000

    Facade should be bound to contract, not a string

--- src/Facades/LicenseKey.php
+++ src/Facades/LicenseKey.php
@@ -2,12 +2,13 @@
 
 namespace DoubleThreeDigital\DigitalProducts\Facades;
 
+use DoubleThreeDigital\DigitalProducts\Contracts\LicenseKeyRepository;
 use Illuminate\Support\Facades\Facade;
 
 class LicenseKey extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return 'LicenseKey';
+        return LicenseKeyRepository::class;
     }
 }
