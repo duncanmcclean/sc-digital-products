78b0ca0ab9eff43203327e9969f75ec6e28fdf88
Duncan McClean <duncan@mcclean.co.uk>
2020-11-06 23:40:50 +0000

    VerificationControllerTest

---
+++ tests/Http/VerificationControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace DoubleThreeDigital\SimpleCommerce\Tests\Http;
+
+use DoubleThreeDigital\DigitalProducts\Facades\LicenseKey;
+use DoubleThreeDigital\DigitalProducts\Tests\TestCase;
+use Statamic\Entries\Entry;
+use Statamic\Entries\EntryCollection;
+use Statamic\Facades\Collection;
+use Statamic\Facades\Entry as FacadesEntry;
+use Statamic\Facades\Stache;
+
+class VerificationControllerTest extends TestCase
+{
+    /** @test */
+    public function can_get_verification_index()
+    {
+        $licenseKey = LicenseKey::generate();
+
+        $collection = Collection::make('orders')->save();
+
+        $order = FacadesEntry::make()
+            ->collection('orders')
+            ->set('is_paid', true)
+            ->set('items', [
+                [
+                    'license_key' => $licenseKey,
+                ],
+            ])
+            ->save();
+
+        $response = $this->postJson('/api/sc-digital-downloads/verification', [
+            'license_key' => $licenseKey,
+        ]);
+
+        $response->assertOk();
+        $response->assertJson([
+            'license_key' => $licenseKey,
+            'valid' => true,
+        ]);
+    }
+}
