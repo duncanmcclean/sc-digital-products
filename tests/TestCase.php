50f44358ab825d6af3ac7c49eafa5cef04b181c8
Duncan McClean <duncan@mcclean.co.uk>
2020-11-06 23:17:03 +0000

    Boot all the Simple Commerce repositories
    Using the service provider makes things weird with addon manifests.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -55,5 +55,28 @@ abstract class TestCase extends OrchestraTestCase
         $app['config']->set('simple-commerce', require(__DIR__.'/../vendor/doublethreedigital/simple-commerce/config/simple-commerce.php'));
 
         Blueprint::setDirectory(__DIR__.'/vendor/doublethreedigital/simple-commerce/resources/blueprints');
+
+        $app->booted(function () use ($app) {
+            $this->bootSimpleCommerceRepositories($app);
+        });
+    }
+
+    protected function bootSimpleCommerceRepositories($app)
+    {
+        collect([
+            \DoubleThreeDigital\SimpleCommerce\Contracts\CartRepository::class => \DoubleThreeDigital\SimpleCommerce\Repositories\CartRepository::class,
+            \DoubleThreeDigital\SimpleCommerce\Contracts\CouponRepository::class => \DoubleThreeDigital\SimpleCommerce\Repositories\CouponRepository::class,
+            \DoubleThreeDigital\SimpleCommerce\Contracts\CurrencyRepository::class => \DoubleThreeDigital\SimpleCommerce\Repositories\CurrencyRepository::class,
+            \DoubleThreeDigital\SimpleCommerce\Contracts\CustomerRepository::class => \DoubleThreeDigital\SimpleCommerce\Repositories\CustomerRepository::class,
+            \DoubleThreeDigital\SimpleCommerce\Contracts\GatewayRepository::class => \DoubleThreeDigital\SimpleCommerce\Repositories\GatewayRepository::class,
+            \DoubleThreeDigital\SimpleCommerce\Contracts\ProductRepository::class => \DoubleThreeDigital\SimpleCommerce\Repositories\ProductRepository::class,
+            \DoubleThreeDigital\SimpleCommerce\Contracts\ShippingRepository::class => \DoubleThreeDigital\SimpleCommerce\Repositories\ShippingRepository::class,
+        ])->each(function ($concrete, $abstract) use ($app) {
+            if (! $app->bound($abstract)) {
+                Statamic::repository($abstract, $concrete);
+            }
+        });
+
+        return $this;
     }
 }
