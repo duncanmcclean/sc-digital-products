ac332b9c378a931378cc94fa751d0e5f20c882d5
Duncan McClean <duncan@mcclean.co.uk>
2020-11-06 19:27:11 +0000

    AddFieldsToProductBlueprintTest

---
+++ tests/Listeners/AddFieldsToProductBlueprintTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace DoubleThreeDigital\DigitalProducts\Tests\Listeners;
+
+use DoubleThreeDigital\DigitalProducts\Listeners\AddFieldsToProductBlueprint;
+use DoubleThreeDigital\DigitalProducts\Tests\TestCase;
+use Statamic\Events\EntryBlueprintFound;
+use Statamic\Facades\Blueprint;
+
+class AddFieldsToProductBlueprintTest extends TestCase
+{
+    /** @test */
+    public function fields_can_be_addded_to_product_blueprint()
+    {
+        $blueprint = Blueprint::make('product')
+            ->setNamespace('collections.products')
+            ->save();
+
+        $event = new EntryBlueprintFound($blueprint);
+
+        $handle = (new AddFieldsToProductBlueprint())->handle($event);
+
+        $this->assertTrue($handle->hasField('is_digital_product'));
+        $this->assertTrue($handle->hasField('downloadable_asset'));
+        $this->assertTrue($handle->hasSection('Digital Product'));
+    }
+
+    /** @test */
+    public function fields_are_not_added_to_orders_blueprint()
+    {
+        $blueprint = Blueprint::make('orders')
+            ->setNamespace('collections.orders')
+            ->save();
+
+        $event = new EntryBlueprintFound($blueprint);
+
+        $handle = (new AddFieldsToProductBlueprint())->handle($event);
+
+        $this->assertFalse($handle->hasField('is_digital_product'));
+        $this->assertFalse($handle->hasField('downloadable_asset'));
+        $this->assertFalse($handle->hasSection('Digital Product'));
+    }
+}
