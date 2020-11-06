a4de7f70b609626d24e2a2ecd9d44fe04607f7c8
Duncan McClean <duncan@mcclean.co.uk>
2020-11-06 18:54:08 +0000

    LicenseKeyRepositoryTest

---
+++ tests/Repositories/LicenseKeyRepositoryTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace DoubleThreeDigital\DigitalProducts\Repositories;
+
+use DoubleThreeDigital\DigitalProducts\Tests\TestCase;
+
+class LicenseKeyRepositoryTest extends TestCase
+{
+    protected $repository;
+
+    public function setUp(): void
+    {
+        $this->repository = resolve(LicenseKeyRepository::class);
+    }
+
+    /** @test */
+    public function can_generate_license_key()
+    {
+        $key = $this->repository->generate();
+
+        $this->assertIsString($key);
+        $this->assertSame(24, strlen($key));
+    }
+}
