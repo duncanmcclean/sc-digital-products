24a740523fa062f67798c81492a9810c5b8199ed
Duncan McClean <duncan@mcclean.co.uk>
2022-04-02 22:32:58 +0100

    Add comment to describe download history config setting

--- config/sc-digital-products.php
+++ config/sc-digital-products.php
@@ -2,6 +2,11 @@
 
 return [
 
+    /*
+     * When enabled, the 'Download History' feature keeps a log of the when downloads for a
+     * specific license take place. The timestamp & IP address of the download are stored.
+     */
+
     'download_history' => true,
 
 ];
