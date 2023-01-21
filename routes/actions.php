73f58549270b84e37612f054f12585254cd84e4f
Duncan McClean <duncan@duncanmcclean.com>
2023-01-21 23:22:54 +0000

    fix import

--- routes/actions.php
+++ routes/actions.php
@@ -1,6 +1,7 @@
 <?php
 
 use DoubleThreeDigital\DigitalProducts\Http\Controllers\DownloadController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/download/{order_id}/{item_id}', [DownloadController::class, 'show'])
     ->name('digital-downloads.download');
