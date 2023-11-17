3c4f4f7445e03bae8f5653c195b4c891016b3198
Duncan McClean <duncan@duncanmcclean.com>
2023-11-17 09:33:54 +0000

    Create an upgrade script to fix existing products

---
+++ src/UpdateScripts/FixDigitalProductToggleForVariantProducts.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DoubleThreeDigital\DigitalProducts\UpdateScripts;
+
+use DoubleThreeDigital\SimpleCommerce\Facades\Product;
+use Statamic\UpdateScripts\UpdateScript;
+
+class FixDigitalProductToggleForVariantProducts extends UpdateScript
+{
+    public function shouldUpdate($newVersion, $oldVersion)
+    {
+        return $this->isUpdatingTo('5.1.0');
+    }
+
+    public function update()
+    {
+        collect(Product::all())
+            ->filter(fn ($entry) => $entry->get('is_digital_product') === true)
+            ->filter(fn ($entry) => $entry->has('product_variants'))
+            ->each(function ($entry) {
+                $productVariants = $entry->get('product_variants');
+
+                $entry
+                    ->merge([
+                        'is_digital_product' => null,
+                        'product_variants' => [
+                        'variants' => $productVariants['variants'],
+                        'options' => collect($productVariants['options'])
+                            ->map(function ($option) {
+                                $option['is_digital_product'] = true;
+
+                                return $option;
+                            })
+                            ->values()
+                            ->toArray(),
+                        ],
+                    ])
+                    ->save();
+            });
+    }
+}
